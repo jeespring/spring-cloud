3e70d1a3c300fae0e733998facb2a0214eb36e03
张朝阳 <jeespring@163.com>
2022-03-12 23:59:11 +0800

    Spring Cloud Hystrix

--- spring-cloud-order/src/main/java/com/jeespring/order/OrderApplication.java
+++ spring-cloud-order/src/main/java/com/jeespring/order/OrderApplication.java
@@ -2,10 +2,12 @@ package com.jeespring.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
