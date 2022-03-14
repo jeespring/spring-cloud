a25edf45c988f5e6f87fc51075216480f660f07c
张朝阳 <jeespring@163.com>
2022-03-14 20:40:11 +0800

    Spring Cloud Hystrix Dashboard

---
+++ spring-cloud-hystrix-dashboard/src/test/java/com/jeespring/hystrix/HystrixDashboardApplicationTests.java
@@ -0,0 +1,19 @@
+package com.jeespring.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * HystrixDashboardApplication
+ *
+ * @author 张朝阳
+ * @date 2022/3/13 17:04:04
+ */
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- spring-cloud-hystrix-order/src/main/java/com/jeespring/order/HystrixOrderApplication.java
+++ spring-cloud-hystrix-order/src/main/java/com/jeespring/order/HystrixOrderApplication.java
@@ -1,10 +1,13 @@
 package com.jeespring.order;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,4 +19,13 @@ public class HystrixOrderApplication {
         SpringApplication.run(HystrixOrderApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
